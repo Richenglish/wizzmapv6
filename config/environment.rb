9176458b19c84ff0489589ac21345a7086a63cd0
Richard English <english.richard@gmail.com>
2013-01-18 16:35:17 -0500

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+WizzmapV6::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WizzmapV6::Application.config.secret_token = '34b82da043b050ae08ce1b65811dc861b5b585b6bbba35065f9d6a60bdc2ebb2e8cfd9e51535fcc50ace22e2e3417a6053c353b71750594f30d546e8e301aa16'
