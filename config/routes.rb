f67d270dcad9679d2178cc2de12d24c9b74eb350
Richard English <english.richard@gmail.com>
2013-01-25 13:16:16 -0500

    custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 WizzmapV6::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+
+  end
+
   resources :points
+  get 'feed', to: 'points#index', as: :feed
+
+
   root to: 'points#index'
 
   # The priority is based upon order of creation:
