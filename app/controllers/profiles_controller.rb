bcfb8415a3eb8555d4b5c7b0f28e69b6cd9cbdc9
Richard English <english.richard@gmail.com>
2013-02-05 09:58:13 -0500

    User map now works

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,19 @@ class ProfilesController < ApplicationController
   	@user = User.find_by_profile_name(params[:id])
   	if @user
   		@points = @user.points.all
-  		render action: :show
+  		
+  	# gmaps4rails code
+
+  		@json = @user.points.all.to_gmaps4rails
+
+  		respond_to do |format|
+      		format.html # index.html.erb
+      		format.json { render json: @points }
+      	end
+	# END of code snippet
+
+
+		#render action: :show  # <-- This is the old code
 
   	else
   	  	render file: 'public/404', status: 404, formats: [:html]
